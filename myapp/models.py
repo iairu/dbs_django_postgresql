e0174f2c2a248bf029d7a36bbf155eae8b27068d
Ondrej Spanik <spanik11@msn.com>
2022-02-27 16:31:47 +0100

    Pridanie JSON endpointu /v1/health, priame SQL

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db import models, connections
+
+
+def _dict_fetch_one(cursor): 
+    # z Django docs https://docs.djangoproject.com/en/4.0/topics/db/sql/#executing-custom-sql-directly
+    columns = [col[0] for col in cursor.description]
+    # return [dict(zip(columns, row)) for row in cursor.fetchall()] # povodne: vsetky zaznamy
+    # return dict(zip(columns, (cursor.fetchone(), cursor.fetchone()))) # dva zaznamy
+    return dict(zip(columns, (cursor.fetchone()))) # jeden zaznam
 
 # Create your models here.
+def sql_query_one(cursor_query: str): 
+    # z Django docs https://docs.djangoproject.com/en/4.0/topics/db/sql/#executing-custom-sql-directly
+    # treba opatrne s SQL, pretoze sa nad nim robia nejake upravy (napr. neukoncuje sa s ;), vid dokumentaciu
+    with connections['readonly'].cursor() as cursor:
+        cursor.execute(cursor_query)
+        out = _dict_fetch_one(cursor)
+    return out
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import sql_query_one # priama SQL podpora
+import json
+
 # Create your views here.
 def index(request):
     return HttpResponse("""
         <h1>Hello FIIT!</h1>
+        <h2>Content</h2>
+        <nav>
+            <a href=\"/v1/health\">/v1/health</a><br>
+        </nav>
         <h2>Relevant stuff</h2>
         <nav>
             <a href=\"https://github.com/FIIT-DBS/zadanie-stv8-binder-iairu\">github repo</a><br>
             <a href=\"https://iairu.com\">iairu.com</a>
         </nav>
     """)
+
+def v1_health(request):
+    # content_type a pod.: https://docs.djangoproject.com/en/4.0/ref/request-response/#id4
+    # django ma namiesto HttpResponse aj JsonResponse, ale mam pocit ze pointa bola precvicit si manualne MIME type nastavenie
+    return HttpResponse(
+        json.dumps({"pgsql":{ # spojenie dvoch dict do jedneho cez unpack operator **
+            **sql_query_one("SELECT VERSION()"), 
+            **sql_query_one("SELECT pg_database_size('dota2')/1024/1024 as dota2_db_size")
+            }}), content_type="application/json; charset=utf-8")
