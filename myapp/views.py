5df7d4cb6b06eb3ce77969472e4b7674ab7879b1
Ondrej Spanik <spanik11@msn.com>
2022-02-27 16:30:55 +0100

    Demonštračný obsah (zatial len index, jednoduché html)

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("""
+        <h1>Hello FIIT!</h1>
+        <h2>Relevant stuff</h2>
+        <nav>
+            <a href=\"https://github.com/FIIT-DBS/zadanie-stv8-binder-iairu\">github repo</a><br>
+            <a href=\"https://iairu.com\">iairu.com</a>
+        </nav>
+    """)
