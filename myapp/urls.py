39b4b7f30a206ea0b2a62101221397c85910d2c9
Ondrej Spanik <spanik11@msn.com>
2022-03-17 23:27:03 +0100

    Pridané nové endpointy pre Z3 (základ)

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('v1/health', views.v1_health, name='v1_health'),
+    path('', views.index),
+    path('v1/health/', views.v1_health),
+    path('v2/patches/', views.v2_patches),
+    path('v2/players/<player_id>/game_exp/', views.v2_players_game_exp),
+    path('v2/players/<player_id>/game_objectives/', views.v2_players_game_objectives),
+    path('v2/players/<player_id>/abilities/', views.v2_players_abilities)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -10,7 +10,11 @@ def index(request):
         <h1>Hello FIIT!</h1>
         <h2>Content</h2>
         <nav>
-            <a href=\"/v1/health\">/v1/health</a><br>
+            <a href=\"/v1/health/\">/v1/health/</a><br>
+            <a href=\"/v2/patches/\">/v2/patches/</a><br>
+            <a href=\"/v2/players/14944/game_exp/\">/v2/players/14944/game_exp/</a><br>
+            <a href=\"/v2/players/14944/game_objectives/\">/v2/players/14944/game_objectives/</a><br>
+            <a href=\"/v2/players/14944/abilities/\">/v2/players/14944/abilities/</a><br>
         </nav>
         <h2>Relevant stuff</h2>
         <nav>
@@ -26,4 +30,93 @@ def v1_health(request):
         json.dumps({"pgsql":{ # spojenie dvoch dict do jedneho cez unpack operator **
             **sql_query_one("SELECT VERSION()"), 
             **sql_query_one("SELECT pg_database_size('dota2')/1024/1024 as dota2_db_size")
-            }}), content_type="application/json; charset=utf-8")
+            }}), content_type="application/json; charset=utf-8", status=200)
+
+def v2_patches(request):
+    # {
+    #   "patches": [
+    #     {
+    #       "patch_version": "6.71",
+    #       "patch_start_date": 1446681600,
+    #       "patch_end_date": 1446768000,
+    #       "matches": [
+    #         {
+    #           "match_id": 0,
+    #           "duration": 39.58
+    #         }
+    #       ]
+    #     }
+    #   ]
+    # }
+    return HttpResponse(
+        json.dumps({"patches":[
+            # **sql_query_one("SELECT VERSION()")
+            ]}), content_type="application/json; charset=utf-8", status=200)
+
+def v2_players_game_exp(request, player_id):
+    # {
+    #   "id": 14944,
+    #   "player_nick": "kakao`",
+    #   "matches": [
+    #     {
+    #       "match_id": 30221,
+    #       "hero_localized_name": "Dark Seer",
+    #       "match_duration_minutes": 52.93,
+    #       "experiences_gained": 25844,
+    #       "level_gained": 22,
+    #       "winner": true
+    #     }
+    #   ]
+    # }
+    return HttpResponse(
+        json.dumps({
+            # **sql_query_one("SELECT VERSION()")
+            "id": player_id # todo: kontrola existencie => status code
+            }), content_type="application/json; charset=utf-8", status=200)
+
+def v2_players_game_objectives(request, player_id):
+    # {
+    #   "id": 14944,
+    #   "player_nick": "kakao`",
+    #   "matches": [
+    #     {
+    #       "match_id": 17730,
+    #       "hero_localized_name": "Slardar",
+    #       "actions": [
+    #         {
+    #           "hero_action": "CHAT_MESSAGE_TOWER_KILL",
+    #           "count": 1
+    #         }
+    #       ]
+    #     }
+    #   ]
+    # }
+    return HttpResponse(
+        json.dumps({
+            # **sql_query_one("SELECT VERSION()")
+            "id": player_id
+            }), content_type="application/json; charset=utf-8", status=200)
+
+def v2_players_abilities(request, player_id):
+    # {
+    #   "id": 14944,
+    #   "player_nick": "kakao`",
+    #   "matches": [
+    #     {
+    #       "match_id": 2980,
+    #       "hero_localized_name": "Slardar",
+    #       "abilities": [
+    #         {
+    #           "ability_name": "slardar_slithereen_crush",
+    #           "count": 1,
+    #           "upgrade_level": 7
+    #         }
+    #       ]
+    #     }
+    #   ]
+    # }
+    return HttpResponse(
+        json.dumps({
+            # **sql_query_one("SELECT VERSION()")
+            "id": player_id
+            }), content_type="application/json; charset=utf-8", status=200)
